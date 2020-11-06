16c92491dcfd2daad35ed452398508ab19e0428a
James Burke <james.walter.burke@gmail.com>
2020-11-06 13:50:36 -0600

    Scaffold templates and prep component map

--- client/irving/componentMap.js
+++ client/irving/componentMap.js
@@ -1,7 +1,11 @@
 import createThemedComponentMap from '@irvingjs/styled-components/componentMap';
+import { componentMap as WordPressComponentMap } from '@irvingjs/wordpress';
+import { IntegrationsManager } from '@irvingjs/integrations';
 
 import themes from 'themes.js';
 
 export default {
   ...createThemedComponentMap(themes),
+  ...WordPressComponentMap,
+  'irving/integrations': IntegrationsManager,
 };
